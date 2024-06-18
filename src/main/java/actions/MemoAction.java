8280fa73514ec1d96fe33fb7a3597531616fbac9
taiki.terada <user116.dounyu@techacademy.jp>
2024-06-18 14:13:29 +0900

    メモeditアクションの作成

--- src/main/java/actions/MemoAction.java
+++ src/main/java/actions/MemoAction.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import actions.views.MemoView;
 import constants.AttributeConst;
 import constants.ForwardConst;
 import constants.JpaConst;
+import constants.MessageConst;
 import services.MemoService;
 
 /**
@@ -47,8 +49,8 @@ public class MemoAction extends ActionBase {
         //全日報データの件数を取得
         long memosCount = service.countAll();
 
-        putRequestScope(AttributeConst.MEMOS, memos); //取得した日報データ
-        putRequestScope(AttributeConst.MEMO_COUNT, memosCount); //全ての日報データの件数
+        putRequestScope(AttributeConst.MEMOS, memos); //取得したメモデータ
+        putRequestScope(AttributeConst.MEMO_COUNT, memosCount); //全てのメモデータの件数
         putRequestScope(AttributeConst.PAGE, page); //ページ数
         putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //1ページに表示するレコードの数
 
@@ -79,6 +81,117 @@ public class MemoAction extends ActionBase {
 
         //新規登録画面を表示
         forward(ForwardConst.FW_MEMO_NEW);
+
+    }
+
+    /**
+     * 新規登録を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void create() throws ServletException, IOException {
+
+        //CSRF対策 tokenのチェック
+        if (checkToken()) {
+
+            //メモの日付が入力されていなければ、今日の日付を設定
+            LocalDate day = null;
+            if (getRequestParam(AttributeConst.REP_DATE) == null
+                    || getRequestParam(AttributeConst.REP_DATE).equals("")) {
+                day = LocalDate.now();
+            } else {
+                day = LocalDate.parse(getRequestParam(AttributeConst.REP_DATE));
+            }
+
+            //セッションからログイン中の従業員情報を取得
+            EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+            //パラメータの値をもとにメモ情報のインスタンスを作成する
+            MemoView mv = new MemoView(
+                    null,
+                    ev, //ログインしている従業員を、日報作成者として登録する
+                    day,
+                    getRequestParam(AttributeConst.MEMO_TITLE),
+                    getRequestParam(AttributeConst.MEMO_CONTENT),
+                    null,
+                    null);
+
+            //日報情報登録
+            List<String> errors = service.create(mv);
+
+            if (errors.size() > 0) {
+                //登録中にエラーがあった場合
+
+                putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+                putRequestScope(AttributeConst.MEMO, mv);//入力された日報情報
+                putRequestScope(AttributeConst.ERR, errors);//エラーのリスト
+
+                //新規登録画面を再表示
+                forward(ForwardConst.FW_MEMO_NEW);
+
+            } else {
+                //登録中にエラーがなかった場合
+
+                //セッションに登録完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERED.getMessage());
+
+                //一覧画面にリダイレクト
+                redirect(ForwardConst.ACT_MEMO, ForwardConst.CMD_INDEX);
+            }
+        }
+    }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException {
+
+        //idを条件に日報データを取得する
+        MemoView mv = service.findOne(toNumber(getRequestParam(AttributeConst.MEMO_ID)));
+
+        if (mv == null) {
+            //該当のメモデータが存在しない場合はエラー画面を表示
+            System.out.println("a");
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            ///ここからエラーです
+            putRequestScope(AttributeConst.MEMO, mv); //取得した日報データ
+
+            //詳細画面を表示
+            forward(ForwardConst.FW_MEMO_SHOW);
+        }
+    }
+
+    /**
+     * 編集画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void edit() throws ServletException, IOException {
+
+        //idを条件に日報データを取得する
+        MemoView mv = service.findOne(toNumber(getRequestParam(AttributeConst.MEMO_ID)));
+
+        //セッションからログイン中の従業員情報を取得
+        EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+        if (mv == null || ev.getId() != mv.getEmployee().getId()) {
+            //該当の日報データが存在しない、または
+            //ログインしている従業員が日報の作成者でない場合はエラー画面を表示
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+
+        } else {
+
+            putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+            putRequestScope(AttributeConst.REPORT, mv); //取得したメモデータ
+
+            //編集画面を表示
+            forward(ForwardConst.FW_MEMO_EDIT);
+        }
 
     }
 
